15bb89fc807411e200d02254b12c3fb9137e4c31
Albin Henningsson <albin.henningsson1@gmail.com>
2023-08-01 09:01:55 +0200

    test stack and some resolver work

--- plox/src/resolver.py
+++ plox/src/resolver.py
@@ -1,17 +1,15 @@
 from typing import Union
 
+from _token import Token
 from expression import Expr, Visitor
-from stmt import BlockStmt, Stmt
+from stack import Stack
+from stmt import BlockStmt, Stmt, VarStmt
 
 
 class Resolver(Visitor):
     def __init__(self, interpreter) -> None:
         self.interpreter = interpreter
-
-    def visit_block_stmt(self, stmt: BlockStmt) -> None:
-        self.__begin_scope()
-        self.__resolve(stmt.statements)
-        self.__end_scope()
+        self.scopes = Stack()
 
     def __resolve(self, res: Union[list[Stmt], Stmt, Expr]) -> None:
         if isinstance(res, list):
@@ -28,3 +26,29 @@ class Resolver(Visitor):
 
     def __begin_scope(self):
         self.scopes.push({})
+
+    def __end_scope(self):
+        self.scopes.pop()
+
+    def visit_block_stmt(self, stmt: BlockStmt) -> None:
+        self.__begin_scope()
+        self.__resolve(stmt.statements)
+        self.__end_scope()
+
+    def visit_var_stmt(self, stmt: VarStmt) -> None:
+        self.__declare(stmt.name)
+        if stmt.initializer != None:
+            self.__resolve(stmt.initializer)
+        self.__define(stmt.name)
+
+    def __declare(self, name: Token):
+        if self.scopes.length == 0:
+            return
+        scope: dict = self.scopes.peek()
+        scope[name.lexeme] = False
+
+    def __define(self, name: Token) -> None:
+        if self.scopes.length == 0:
+            return
+
+        self.scopes.peek()[name] = True
---
+++ plox/tests/test_stack.py
@@ -0,0 +1,27 @@
+import sys
+
+sys.path.append("../src/")
+from stack import Stack
+
+
+def test_stack():
+    st = Stack()
+
+    st.push(5)
+    st.push(7)
+    st.push(9)
+
+    assert st.pop() == 9
+    assert st.length == 2
+
+    st.push(11);
+
+    assert st.pop() == 11 
+    assert st.pop() == 7 
+    assert st.peek() == 5 
+    assert st.pop() == 5 
+    assert st.pop() == None 
+
+    st.push(69)
+    assert st.peek() == 69
+    assert st.length == 1

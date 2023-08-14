2c92f792340a9778c0ea641f88fe1431653b936e
Albin Henningsson <albin.henningsson1@gmail.com>
2023-08-14 16:14:17 +0200

    Chapter 19 strings

--- clox/vm.c
+++ clox/vm.c
@@ -1,9 +1,12 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "common.h"
 #include "compiler.h"
 #include "debug.h"
+#include "object.h"
+#include "memory.h"
 #include "vm.h"
 
 VM vm;
@@ -23,8 +26,13 @@ static void runTimeError(const char *format, ...) {
   resetStack();
 }
 
-void initVM() { resetStack(); }
-void freeVM() {}
+void initVM() { 
+  resetStack(); 
+  vm.objects = NULL;
+}
+void freeVM() {
+  freeObjects();
+}
 
 void push(Value value) {
   *vm.stackTop = value;
@@ -40,6 +48,20 @@ static Value peek(int distance) { return vm.stackTop[-1 - distance]; }
 
 static bool isFalsey(Value value) {
   return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
+static void concatenate() {
+  ObjString *b = AS_STRING(pop());
+  ObjString *a = AS_STRING(pop());
+
+  int length = a->length + b->length;
+  char *chars = ALLOCATE(char, length + 1);
+  memcpy(chars, a->chars, a->length);
+  memcpy(chars + a->length, b->chars, b->length);
+  chars[length] = '\0';
+
+  ObjString *result = takeString(chars, length);
+  push(OBJ_VAL(result));
 }
 
 static InterpretResult run() {
@@ -92,16 +114,25 @@ static InterpretResult run() {
       push(BOOL_VAL(valuesEqual(a, b)));
       break;
     }
-    case OP_GREATER:{
-        BINARY_OP(BOOL_VAL, >);
-        break;
-      }
-    case OP_LESS:{
-        BINARY_OP(BOOL_VAL, <);
-        break;
-      }
+    case OP_GREATER: {
+      BINARY_OP(BOOL_VAL, >);
+      break;
+    }
+    case OP_LESS: {
+      BINARY_OP(BOOL_VAL, <);
+      break;
+    }
     case OP_ADD: {
-      BINARY_OP(NUMBER_VAL, +);
+      if (IS_STRING(peek(0)) && IS_STRING(peek(1))) {
+        concatenate();
+      } else if (IS_NUMBER(peek(0)) && IS_NUMBER(peek(1))) {
+        double b = AS_NUMBER(pop());
+        double a = AS_NUMBER(pop());
+        push(NUMBER_VAL(a + b));
+      } else {
+        runTimeError("Operands must be two numbers or two strings.");
+        return INTERPRET_RUNTIME_ERROR;
+      }
       break;
     }
     case OP_SUBTRACT: {

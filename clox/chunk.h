726260f4ab6a35d10815371ae86f99955fd7c46a
albinhsn <albin.henningsson1@gmail.com>
2023-08-14 13:51:09 +0200

    chapter 15-17

---
+++ clox/chunk.h
@@ -0,0 +1,31 @@
+#ifndef clox_chunk_h
+#define clox_chunk_h
+
+#include "value.h"
+#include "common.h"
+
+typedef enum {
+  OP_RETURN,
+  OP_CONSTANT,
+  OP_ADD,
+  OP_SUBTRACT,
+  OP_MULTIPLY,
+  OP_DIVIDE,
+  OP_NEGATE,
+} OpCode;
+
+typedef struct {
+  int count;
+  int capacity;
+  uint8_t *code;
+  int*lines;
+  ValueArray constants;
+} Chunk;
+
+void initChunk(Chunk *chunk);
+void freeChunk(Chunk *chunk);
+void writeChunk(Chunk *chunk, uint8_t byte, int line);
+
+int addConstant(Chunk *chunk, Value value);
+
+#endif // clox_chunk_h
